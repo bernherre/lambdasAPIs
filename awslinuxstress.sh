168b176eda9797f593e3c3c3eac60908ba1e6154
Bernardo H <63518205+bernherre@users.noreply.github.com>
2025-01-02 12:36:30 -0500

    Create awslinuxstress.sh
    stress Amazon linux 2 server or instance

---
+++ awslinuxstress.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Use this for your user data (script from top to bottom)
+# install stress (Linux 2 version)
+sudo amazon-linux-extras install epel -y
+sudo yum install stress -y 
+
+#stress -c #of cpus
